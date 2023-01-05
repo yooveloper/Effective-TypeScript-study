4c282f740eac3f25c971a2b06d8f47833c907939
이도현 <9598dohyun@naver.com>
2023-01-05 19:16:12 +0900

    add dohyun

---
+++ WEEK2/ex/do.tsx
@@ -0,0 +1,7 @@
+enum Brand {
+    Nike = 'nike',
+    Adidas = 'adidas',
+    Puma = 'puma'
+}
+let type : typeof Brand = {Nike: Brand.Nike, Adidas: Brand.Adidas, Puma: Brand.Puma};
+// let type: keyof typeof Brand
